b1a5da9ca0dc6b2526f97b2f8083e51cd1c490ec
Anil Nimbel <Anil.Nimbel@civica.com>
2025-06-13 17:55:07 +0530

    Graph

---
+++ Controllers/GraphController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ADOAnalyser.Controllers
+{
+    public class GraphController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
