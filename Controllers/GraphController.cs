dedc195e74cfd845bd5917645e77d025f5a7deb7
Digvijay Zala <Digvijay.Zala@civica2.com>
2025-06-16 16:39:18 +0530

    Resolved issue

--- Controllers/GraphController.cs
+++ Controllers/GraphController.cs
@@ -1,12 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ADOAnalyser.DBContext;
+using ADOAnalyser.Models;
+using ADOAnalyser.PipelineModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using static ADOAnalyser.Controllers.BuildController;
 
 namespace ADOAnalyser.Controllers
 {
     public class GraphController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public GraphController(AppDbContext context)
+        {
+            _context = context;            
+        }
         public IActionResult Index()
         {
-            return View();
+            var pipelineJson = _context.TestRunResults
+                  .OrderByDescending(r => r.RunDate).Take(5)
+                  .ToList();
+            var definitions = new List<GraphDefinition>();
+
+
+
+            if (pipelineJson.Any() == true)
+            {
+                foreach (var pipeline in pipelineJson)
+                {
+                    int number = Regex.Matches(pipeline.ResultSummary, @"\d+")
+                    .Cast<Match>()
+                    .Select(m => int.Parse(m.Value))
+                    .FirstOrDefault();
+
+
+                    definitions.Add(new GraphDefinition
+                    {
+                        Date = pipeline.StartDate?.ToShortDateString() + "-" + pipeline.EndDate?.ToShortDateString(),
+                        Count = number
+                    });
+                }
+            }
+
+            return View(definitions);
+        }
+
+        public class GraphDefinition
+        {
+            public string Date { get; set; }
+            public int Count { get; set; }
         }
     }
 }
