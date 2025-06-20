60df2870a02b2af205112a1b2f0db700b265a577
Digvijay Zala <Digvijay.Zala@civica2.com>
2025-06-20 14:53:50 +0530

    New Data table

---
+++ Database/Create_EmailConfig.sql
@@ -0,0 +1,24 @@
+USE [AdoChecker]
+GO
+
+/****** Object:  Table [dbo].[EmailConfig]    Script Date: 6/2/2025 12:22:43 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[EmailConfig](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[EmailIds] [nvarchar](max) NULL,
+	[CreatedDate] DateTime NULL,
+	[ModifiedDate] DateTime NULL,
+	[IsActive] bit Default(1),
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
